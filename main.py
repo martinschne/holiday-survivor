dae058b49899f971c0917200814b12e40fa72e3b
Philipp Spitzley <philipp@Mac-mini-von-Philipp.fritz.box>
2025-01-22 11:53:37 +0100

    add docstrings and re-format imports of main.py

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import sys
-from sms_polling import poll_sms_messages
+
+from json_storage import JSONStorage
 from registration_mgnt import register_new_user
 from sending_msg_user import sending_msg_user
-from json_storage import JSONStorage
+from sms_polling import poll_sms_messages
 
 storage = JSONStorage("holiday_survivors_json.json")
 
 
 def introduction_display():
+    """
+    Displays the introduction of the program, including the name of the application and the team name.
+
+    :returns: None
+    """
     print("-----------Welcome to the Holiday Survivors!-----------")
     print("-----------------Team HolidaySurvivors-----------------")
     print("-------------------------------------------------------")
 
 
 def menu_display():
+    """
+    Displays the menu of the program, including the options to register a new user, to send a custom message to user, to get message, and to quit the program.
+
+    :returns: None
+    """
     menu_content = """
             1. Register new user
             2. Sending a custom message to user
@@ -25,13 +36,20 @@ def menu_display():
 
 def menu_dispatch():
     # create a menu dict
+    """
+    Displays the menu of the program, including the options to register a new user, to send a custom message to user, to get message, and to quit the program.
+
+    The function takes no arguments, and it will keep asking the user for input until it receives a valid number between 1 and the length of the menu options.
+
+    :raises ValueError: If the user enters a number that is not between 1 and the length of the menu options.
+    :returns: None
+    """
     menu_option = {
         "1": command_register_new_user,
-        # "2": command_unregister_user,
+        # "2": command_unregister_user, # currently not in use
         "2": command_sending_custom_msg_to_user,
         "3": command_get_msg,
-        "4": command_quit_program
-
+        "4": command_quit_program,
     }
 
     # print menu content
@@ -40,14 +58,20 @@ def menu_dispatch():
     # handling user input
     while True:
         try:
-            user_input = int(input(f"Please enter a number (1-{len(menu_option)}) to choose an option below:"))
+            user_input = int(
+                input(
+                    f"Please enter a number (1-{len(menu_option)}) to choose an option below:"
+                )
+            )
             if user_input < 1 or user_input > len(menu_option):
                 raise ValueError(
-                    f"Please only enter a number between 1-{len(menu_option)} (1 and {len(menu_option)} included)")
+                    f"Please only enter a number between 1-{len(menu_option)} (1 and {len(menu_option)} included)"
+                )
         except ValueError as e:
             print(
-                f"Please only enter a number between 1 and {len(menu_option)} (1 and {len(menu_option)} included):" + str(
-                    e))
+                f"Please only enter a number between 1 and {len(menu_option)} (1 and {len(menu_option)} included):"
+                + str(e)
+            )
         else:
             break
     # invoke the chosen function
@@ -59,13 +83,19 @@ def command_get_msg():
 
 
 def command_register_new_user():
+    """
+    Prompts user to input a phone number and registers the number to the service.
+    """
     print("-----------Register a new user!-----------")
     print("------------------------------------------")
-    user_input = input("Please enter the phone number (enter prefix number following phone number):")
+    user_input = input(
+        "Please enter the phone number (enter prefix number following phone number):"
+    )
     register_new_user(user_input)
     print("------------------------------------------")
 
 
+# currently not in use
 """
 def command_unregister_user():
     print("-----------Unregister a new user!-----------")
@@ -77,6 +107,9 @@ def command_unregister_user():
 
 
 def command_sending_custom_msg_to_user():
+    """
+    Prompts user to input a phone number and a message, and sends the message to the user.
+    """
     print("-----------Sending custom msg to user!-----------")
     print("-------------------------------------------------")
     user_input_number = input("Enter a phone number of the receiver:")
@@ -86,12 +119,24 @@ def command_sending_custom_msg_to_user():
 
 
 def command_quit_program():
+    """
+    Prints a goodbye message and exits the program.
+
+    This function is invoked by the menu option 4, "Quit program".
+    """
     print("----------------See you next time!---------------")
     print("-------------------------------------------------")
     sys.exit()
 
 
 def main():
+    """
+    The main entry point of the program.
+
+    This function first displays an introduction, and then enters an infinite loop,
+    where it displays the menu and invokes the chosen function until the user chooses
+    to quit the program.
+    """
     introduction_display()
     while True:
         menu_dispatch()
