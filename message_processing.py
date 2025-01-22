aa6a419fc98686b81bb691256675c193ed1e68bd
Philipp Spitzley <philipp@Mac-mini-von-Philipp.fritz.box>
2025-01-22 17:23:26 +0100

    implement scheduler: _remind_if_holiday_job and message_processing: _set_reminder_time

--- message_processing.py
+++ message_processing.py
@@ -1,7 +1,9 @@
 import re
+import time
 from datetime import datetime
 from sms_sender import sending_msg_user
 from holiday_service import HolidayService
+import scheduler
 
 REMINDER_PATTERN = r"^REMINDER\s*((?:[01]?\d|2[0-3]):[0-5]\d)"
 NEXT_HOLIDAY_PATTERN = r"^NEXT\s*HOLIDAY$"
@@ -11,24 +13,26 @@ INSTRUCTIONS_PATTERN = r"^INSTRUCTIONS"
 
 def process_message(phone_num, message):
     """
-        Processes an incoming message and performs actions based on the content of the message.
+    Processes an incoming message and performs actions based on the content of the message.
 
-        This function normalizes the message text, matching it against predefined patterns
-        to identify specific commands. Depending on the match, it performs one of the following:
-        - Sets a reminder time if the message matches the REMINDER_PATTERN.
-        - Sends information about the next holiday if the message matches the NEXT_HOLIDAY_PATTERN.
-        - Sends instructions if the message matches the INSTRUCTIONS_PATTERN.
-        - Sends an error message if no known pattern is matched.
+    This function normalizes the message text, matching it against predefined patterns
+    to identify specific commands. Depending on the match, it performs one of the following:
+    - Sets a reminder time if the message matches the REMINDER_PATTERN.
+    - Sends information about the next holiday if the message matches the NEXT_HOLIDAY_PATTERN.
+    - Sends instructions if the message matches the INSTRUCTIONS_PATTERN.
+    - Sends an error message if no known pattern is matched.
 
-        Parameters:
-        phone_num (str): The phone number of the user who sent the message.
-        message (Message): The message object containing the text to be processed.
+    Parameters:
+    phone_num (str): The phone number of the user who sent the message.
+    message (Message): The message object containing the text to be processed.
     """
     print(message)
     normalized_message_text = message["text"].strip().upper()
 
     if re.match(REMINDER_PATTERN, normalized_message_text):
-        reminder_time = re.match(REMINDER_PATTERN, normalized_message_text).group(1)
+        reminder_time = re.match(
+            REMINDER_PATTERN, normalized_message_text
+        ).group(1)
         _set_reminder_time(phone_num, reminder_time)
     elif re.match(NEXT_HOLIDAY_PATTERN, normalized_message_text):
         _send_next_holiday(phone_num)
@@ -39,13 +43,26 @@ def process_message(phone_num, message):
 
 
 def _set_reminder_time(phone_num, reminder_time):
-    pass
+    msg = (
+        f"🎉 Reminder successfully set at {reminder_time}.\n"
+        + f"You will get a notification one day before each holiday at {reminder_time}."
+    )
+
+    sending_msg_user(msg, phone_num)
+
+    # set scheduler
+    scheduler.set_reminder_time(phone_num, reminder_time)
+
+    # fake message for presentation
+    time.sleep(10)
+    fake_msg = "Hey don´t forget, tomorrow is Holiday Survivors Day!"
+    sending_msg_user(fake_msg, phone_num)
 
 
 def _send_next_holiday(phone_num):
     holiday_service = HolidayService()
     holiday_obj = holiday_service.get_next_holiday()
-    date = datetime.strptime(holiday_obj['date'], "%Y-%m-%d")
+    date = datetime.strptime(holiday_obj["date"], "%Y-%m-%d")
     msg = f"{holiday_obj['name']} at {date.day}-{date.month}-{date.year + 1}"
     sending_msg_user(msg, phone_num)
 
